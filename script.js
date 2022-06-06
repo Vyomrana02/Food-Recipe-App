d9e2f72fea5871e104ceee6fcdfc33a1139997d2
Vyomrana02 <vyom.rana02@gmail.com>
2022-06-06 19:38:34 +0530

    url change

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 let result = document.getElementById("result");
 let searchBtn = document.getElementById("search-btn");
-let url = "https://www.themealdb.com/api/json/v1/1/search.php?s=";
+let url = "https://www.themealdb.com/api/json/v1/1/search.php?s= ";
 
 searchBtn.addEventListener("click", () => {
   let userInp = document.getElementById("user-inp").value;
   if (userInp.length == 0) {
     result.innerHTML = `<h3>Input Field Cannot Be Empty</h3>`;
   } else {
-    fetch(url+userInp)
+    fetch(url + userInp)
       .then((response) => response.json())
       .then((data) => {
         let myMeal = data.meals[0];
