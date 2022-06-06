5bf8b2be37360e376862949bd555b6c470a20ccc
Vyomrana02 <vyom.rana02@gmail.com>
2022-06-06 20:02:41 +0530

    commit

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ let result = document.getElementById("result");
 let searchBtn = document.getElementById("search-btn");
 let url = "https://www.themealdb.com/api/json/v1/1/search.php?s=";
 
+
 searchBtn.addEventListener("click", () => {
   let userInp = document.getElementById("user-inp").value;
   if (userInp.length == 0) {
